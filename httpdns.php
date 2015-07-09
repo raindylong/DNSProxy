85fe2b979d1bc093e35b2850b44b3b8a5777e8ca
longweijin <lwj@ejoy.com>
2015-07-09 17:56:44 +0800

    array exist

--- httpdns.php
+++ httpdns.php
@@ -2,21 +2,79 @@
 // 获取链接的HTML代码
 //
 $domain = $_GET["domain"];
+
+$domain = str_replace("http://","", $domain) ;
+
+$domain = str_replace(":","", $domain) ;
+$domain = str_replace("&","", $domain) ;
+$domain = str_replace(";","", $domain) ;
+$domain = str_replace("@","", $domain) ;
+$domain = str_replace("|","", $domain) ;
+$domain = str_replace("!","", $domain) ;
+$domain = str_replace(",","", $domain) ;
+$domain = str_replace("#","", $domain) ;
+$domain = str_replace("$","", $domain) ;
+$domain = str_replace("%","", $domain) ;
+$domain = str_replace("*","", $domain) ;
+$domain = str_replace("^","", $domain) ;
+$domain = str_replace("(","", $domain) ;
+$domain = str_replace(")","", $domain) ;
+$domain = str_replace("+","", $domain) ;
+$domain = str_replace("=","", $domain) ;
+$domain = str_replace("[","", $domain) ;
+$domain = str_replace("]","", $domain) ;
+$domain = str_replace("{","", $domain) ;
+$domain = str_replace("}","", $domain) ;
+$domain = str_replace("\"","", $domain) ;
+$domain = str_replace("'","", $domain) ;
+$domain = str_replace("/","", $domain) ;
+
+
+if ( $domain == "" ) {
+	print_r("127.0.0.1");exit;
+}
+
+// 如果拿到的domain最后一个字符不是"."，则增加一个"."
+$arrp = str_split($domain);
+if( $arrp[count($arrp)-1] != '.' ){
+	$domain = $domain.'.';
+};
+
+$search_array = array(
+///////////// 以下是数组内容 /////
+"init.gc.apple.com."=>"96.7.54.185",
+"cn.wsj.com."=>"96.7.54.98",
+///////////// 以上是数组内容 /////////////
+) ;
+
+
+if (array_key_exists("$domain", $search_array)) {
+        print_r($search_array["$domain"]);
+        exit;
+
+}
+
+
 $html = file_get_contents("http://119.29.29.29/d?dn=$domain");
 
 if(!$html){
-	die("");
+        die("");
 }
 
-//print_r("----");
-//print_r($domain."\n");
-//如果有多个结果，只取一个IP返回
+function wdomain($ip,$string){
+        $fp = fopen("./domain/$string.txt", "w");//文件被清空后再写入
+        $flag=fwrite($fp,"$ip $string\n");
+        fclose($fp);
+}
+
 $array = explode(";",$html);
-if($array[1]== ""){
-	print_r($html);
+if( isset($array[1]) && ($array[1] != "") ){
+        wdomain($array[1],$domain);
+        print_r($array[1]);
 }
 else{
-	print_r($array[1]);
+        wdomain($html,$domain);
+	print_r($html);
 }
 
 ?>
