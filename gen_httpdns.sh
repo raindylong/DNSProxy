a9035c39755ba56a55936e8d8ab9fbef8577fa64
longweijin <lwj@ejoy.com>
2015-07-11 01:03:52 +0800

    定时删除文件缓存

--- gen_httpdns.sh
+++ gen_httpdns.sh
@@ -16,6 +16,14 @@ if [ ! -d ${domain_tmpdir} ] ; then
 	touch ${domain_tmp}/domain.txt
 fi
 
+ls -ltr /tmp/domain/* | awk '{print $NF}' > /tmp/domain_dnsproxy.txt
+WCC=`wc -l /tmp/domain_dnsproxy.txt | awk '{print $1}'`
+
+if [ ${WCC} -ge 4000 ] ; then
+	head -1000 /tmp/domain_dnsproxy.txt | xargs rm 
+
+fi
+
 cat /etc/hosts ${domain_tmpdir}/*.txt | awk '{ print $1" "$2}' | sort | uniq | \
 	 awk '{ if(NF=2){ print "\""$2".\"=>\""$1"\"," } }' | sed "s/\.\././g" >> dns_tmp.php
 
