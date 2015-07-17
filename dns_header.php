40334c4dfb762ffbdaf41c8105611894b5b6402c
longweijin <lwj@ejoy.com>
2015-07-17 18:34:47 +0800

    mod3

--- dns_header.php
+++ dns_header.php
@@ -4,20 +4,21 @@ $domain = $_GET["domain"];
 
 $domain = str_replace("http://","", $domain) ;
 
-
 // 如果是DNS反向解析请求，直接返回一个域名
 $arpa = explode("-",$domain);
 if ( $arpa[count($arpa)-1] == "addr.arpa." ){
         print_r("addr.arpa.ejoy.com");exit;
 }
 
+# SOMEGUY-PC._netbios._udp.WORKGROUP.
 
 # SOMEGUY-PC._netbios._udp.WORKGROUP.
 
 $workgroup = explode("_netbios._udp.",$domain);
-if (( $workgroup[1] == 'WORKGROUP.' ) || ( $workgroup[1] == 'WOKRGROUP.' )) {
+if (( $workgroup[1] == 'WORKGROUP.' ) || ( $workgroup[1] == 'WOKRGROUP.' ) || ( $workgroup[1] == 'workgroup.' )) {
         print_r("127.0.0.1");exit;
 }
+
 
 $domain = str_replace(":","", $domain) ;
 $domain = str_replace("&","", $domain) ;
